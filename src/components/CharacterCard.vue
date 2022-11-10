e2ae87b4c6aedcb105d4f5e5b7a5a8dcbf2ec31d
luishs0 <luishs.doc@gmail.com>
2022-11-10 14:04:49 +0100

    first commit

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "CharacterCard",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/CharactersList.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from "../store";
+import CharacterCard from "./CharacterCard.vue";
+
+export default {
+    name: "CharactersList",
+    data() {
+        return {
+            store: store,
+        }
+    },
+    components: {
+        CharacterCard,
+    }
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
