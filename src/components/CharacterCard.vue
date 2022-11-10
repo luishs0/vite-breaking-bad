c4a4e64e3c7b0d92c6fcaef14f2b13f92d9477f6
luishs0 <luishs.doc@gmail.com>
2022-11-10 15:49:33 +0100

    aggiunto style con griglia statica

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -11,14 +11,16 @@ export default {
 
 
 <template>
-    <div class="ms_card">
+    <div class="ms_card mb-3">
         <div class="card-img">
             <img src="https://images.squarespace-cdn.com/content/v1/56a5f5ef5827c35bf58e8e02/1517306531940-MSJ88JW0TQR1WNLNCZK5/Algo_Logo.jpeg"
                 alt="">
         </div>
 
-        <div class="card-text">
-            Algo
+        <div class="card-text text-center">
+            <div class="name mt-3 mb-3">Name</div>
+            <div class="title mt-1 mb-1">Breaking Bad</div>
+            <div class="status mt-1 mb-1">Status</div>
         </div>
     </div>
 </template>
@@ -27,9 +29,23 @@ export default {
 <style lang="scss" scoped>
 .ms_card {
     width: calc(100% / 5 - 1rem);
+    background-color: #707070;
 }
 
 .card-img {
     width: 100%;
+}
+
+.name {
+    color: white;
+}
+
+.title,
+.status {
+    color: #cfcfcf;
+}
+
+img {
+    padding: .5rem;
 }
 </style>
