42dc1cfa5966dc0441183c669da55b63a2e21505
luishs0 <luishs.doc@gmail.com>
2022-11-11 18:35:39 +0100

    finito

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,33 @@ import { store } from "./store";
 export default {
   data() {
     return {
-      store
+      store,
     }
   },
   components: {
     CharactersList
   },
   created() {
-    axios.get("https://www.breakingbadapi.com/api/characters").then((resp) => {
-      this.store.characters = resp.data;
-      console.log(this.store.characters);
-    });
+    this.getCharacters();
+  },
+  methods: {
+    getCharacters() {
+      let apiUrl = "";
+      if (this.store.selected === "Breaking Bad") {
+        apiUrl = "https://www.breakingbadapi.com/api/characters?category=Breaking+Bad";
+      } else if (this.store.selected === "Better Call Saul") {
+        apiUrl = "https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul";
+      } else {
+        apiUrl = "https://www.breakingbadapi.com/api/characters";
+      }
+
+      axios.get(apiUrl).then((resp) => {
+        this.store.characters = resp.data;
+        console.log(this.store.characters);
+      });
+
+
+    }
   }
 }
 
@@ -24,6 +40,14 @@ export default {
 
 
 <template>
+  <div class="container pt-3 pb-3">
+    <select name="select" id="select" v-model="store.selected">
+      <option value="" selected>All</option>
+      <option value="Breaking Bad">Breaking Bad</option>
+      <option value="Better Call Saul">Better Call Saul</option>
+    </select>
+    <button class="btn btn-primary" @click="getCharacters">Search</button>
+  </div>
   <CharactersList />
 </template>
 
